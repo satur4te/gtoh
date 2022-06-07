a9b44ee2e29c01243f57a428b7e42e8fbcf199c4
wrkhd <ne2erm0re@gmail.com>
2022-06-07 23:45:07 +0000

    extended gemparser, need comments

--- gemparser.cpp
+++ gemparser.cpp
@@ -6,23 +6,41 @@
 #include <regex>
 #include <iostream>
 #include "gemparser.h"
-
+//"^```(.*)"
 
 void gemparser::parse(const std::string& in_path, const std::string& out_path) {
     std::ifstream infile(in_path);
     std::ofstream outfile(out_path);
-    const std::string init_regex_array[6] = {"^# (.*)", "^## (.*)", "^### (.*)","^\\* (.*)", "^=> (.*?) (.*)", "^[^<].*"};
-    const std::string substitute_regex_array[6] = {"<h1>$1</h1>", "<h2>$1</h2>", "<h3>$1</h3>", "<li>$1</li>", "<a href='$1'>$2</a>", "<p>$0</p>"};
+    const std::string init_regex_array[8] = {"^# (.*)", "^## (.*)", "^### (.*)", "^\\* (.*)",
+                                             "^=> (.*?) (.*)", "^> (.*)", "^[^<].*"};
+
+    const std::string substitute_regex_array[8] = {"<h1>$1</h1>", "<h2>$1</h2>", "<h3>$1</h3>","<li>$1</li>",
+                                                   "<a href='$1'>$2</a>", "<blockquote>$1</blockquote>","<p>$0</p>"};
 
     if (infile.is_open() && outfile.is_open()) {
         std::string line;
+        int pre_count = 0;
 
         while (std::getline(infile, line)) {
 
-            for(int i = 0; i < 6; i++) {
-                line = std::regex_replace(line, std::regex(init_regex_array[i]), substitute_regex_array[i]);
+            if(std::regex_match(line, std::regex("^```(.*)"))) {
+                if(pre_count == 0) {
+                    outfile << "<pre>" << std::endl;
+                    pre_count++;
+                    continue;
+                }
+
+                else if(pre_count == 1) {
+                    outfile << "</pre>" << std::endl;
+                    pre_count = 0;
+                    continue;
+                }
             }
-            std::cout << line << std::endl;
+
+            if(pre_count == 0) {
+                for (int i = 0; i < 7; i++)
+                    line = std::regex_replace(line, std::regex(init_regex_array[i]), substitute_regex_array[i]);
+            }
 
             outfile << line << std::endl;
         }
--- main.cpp
+++ main.cpp
@@ -1,22 +1,16 @@
 #include <string>
 #include <iostream>
-#include <filesystem>
-#include <regex>
-#include "gemparser.h"
 #include "fdparser.h"
-
-namespace fs = std::filesystem;
 
 int main() {
     std::string in_dir, out_dir;
-    in_dir = "/home/wrkhd/Workspace/gemtexttohtml/testin";
-    out_dir = "/home/wrkhd/Workspace/gemtexttohtml/testout";
+    //in_dir = "/home/wrkhd/Workspace/gemtexttohtml/testin";
+    //out_dir = "/home/wrkhd/Workspace/gemtexttohtml/testout";
     std::cout << "Initial path: " << std::endl;
-    //std::cin >> in_dir;
+    std::cin >> in_dir;
     std::cout << "Output path: " << std::endl;
-    //std::cin >> out_dir;
-    fdparser::parse("/home/wrkhd/Workspace/gemtexttohtml/testin", "/home/wrkhd/Workspace/gemtexttohtml/testout");
-    // gemparser::parse("/home/wrkhd/Workspace/1.gmi", "/home/wrkhd/Workspace/1.html");
+    std::cin >> out_dir;
+    fdparser::parse(in_dir, out_dir);
 
     return 0;
 }
